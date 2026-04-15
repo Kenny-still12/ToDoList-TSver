12b8e4695fa3b8f05135a22c8d8f44be4e1b122b
Kenneth Sonoda <114664532+Kenny-still12@users.noreply.github.com>
2026-04-16 08:26:29 +1000

    feat: enhance todo list functionality with dynamic item creation and display

--- dist/app.js
+++ dist/app.js
@@ -2,6 +2,7 @@
 const form = document.querySelector("form");
 const submitbtn = document.querySelector(".add-btn");
 const input = document.querySelector(".input-list");
+const listContainer = document.querySelector(".list-container");
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     let todoText = input === null || input === void 0 ? void 0 : input.value;
@@ -10,9 +11,17 @@ form.addEventListener("submit", (e) => {
         text: todoText,
         isComplete: false
     };
+    addList(newList);
 });
 function addList(listHolder) {
     const list = document.createElement("div");
+    const listText = document.createElement("p");
+    list.classList.add("list-holder");
+    if (typeof listHolder.text === "string") {
+        listText.innerText = listHolder.text;
+    }
+    list.append(listText);
+    listContainer.append(list);
 }
 export {};
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@
 const form = document.querySelector("form") as HTMLFormElement;
 const submitbtn = document.querySelector(".add-btn");
 const input = document.querySelector(".input-list") as HTMLInputElement;
+const listContainer = document.querySelector(".list-container") as HTMLDivElement;
 
 type todoList = {
     id: string,
@@ -20,10 +21,18 @@ form!.addEventListener("submit", (e: Event) => {
         text: todoText,
         isComplete: false
     }
+    addList(newList);
 
 });
 
 function addList(listHolder: todoList) {
-    const list = document.createElement("div");
+    const list = document.createElement("div") as HTMLDivElement;
+    const listText = document.createElement("p") as HTMLParagraphElement;
+    list.classList.add("list-holder");
+    if (typeof listHolder.text === "string") {
+        listText.innerText = listHolder.text;
+    }
 
+    list.append(listText);
+    listContainer.append(list);
 }
