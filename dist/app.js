1ff0c5c5b14c5a24773f56b2c1e3c4be8f15d6b5
Kenneth Sonoda <114664532+Kenny-still12@users.noreply.github.com>
2026-04-23 18:12:52 +1000

    feat: add delete functionality to todo list items

--- dist/app.js
+++ dist/app.js
@@ -3,6 +3,7 @@ const form = document.querySelector("form");
 const submitbtn = document.querySelector(".add-btn");
 const input = document.querySelector(".input-list");
 const listContainer = document.querySelector(".list-container");
+const deleteBtn = document.querySelector(".delete-btn");
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     let todoText = input === null || input === void 0 ? void 0 : input.value;
@@ -17,11 +18,18 @@ function addList(listHolder) {
     const list = document.createElement("div");
     const listText = document.createElement("p");
     const deletebtn = document.createElement("button");
+    deletebtn.classList.add("delete-btn");
     list.classList.add("list-holder");
     deletebtn.innerHTML = "delete";
+    list.dataset.id = listHolder.id;
     if (typeof listHolder.text === "string") {
         listText.innerText = listHolder.text;
     }
+    deletebtn.addEventListener("click", (e) => {
+        var _a;
+        const button = e.currentTarget;
+        (_a = button.parentElement) === null || _a === void 0 ? void 0 : _a.remove();
+    });
     list.append(listText);
     list.append(deletebtn);
     listContainer.append(list);
--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ const form = document.querySelector("form") as HTMLFormElement;
 const submitbtn = document.querySelector(".add-btn");
 const input = document.querySelector(".input-list") as HTMLInputElement;
 const listContainer = document.querySelector(".list-container") as HTMLDivElement;
+const deleteBtn = document.querySelector(".delete-btn") as HTMLButtonElement;
 
 type todoList = {
     id: string,
@@ -25,19 +26,29 @@ form!.addEventListener("submit", (e: Event) => {
 
 });
 
+
 function addList(listHolder: todoList) {
     const list = document.createElement("div") as HTMLDivElement;
     const listText = document.createElement("p") as HTMLParagraphElement;
     const deletebtn = document.createElement("button") as HTMLButtonElement;
+    deletebtn.classList.add("delete-btn");
     list.classList.add("list-holder");
 
     deletebtn.innerHTML = "delete";
+
+    list.dataset.id = listHolder.id;
 
     if (typeof listHolder.text === "string") {
         listText.innerText = listHolder.text;
     }
 
+    deletebtn.addEventListener("click", (e: Event) => {
+        const button = e.currentTarget as HTMLButtonElement;
+        button.parentElement?.remove();
+    })
+
     list.append(listText);
     list.append(deletebtn);
     listContainer.append(list);
 }
+
